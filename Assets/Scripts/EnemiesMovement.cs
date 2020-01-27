96ac8c205e8ddfaa56433167471919a9c4acd0c6
malloryxdd <paumedinamartin@gmail.com>
2020-01-27 13:17:47 +0100

    IA enemigos (creo)

---
+++ Assets/Scripts/EnemiesMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesMovement : MonoBehaviour
+{
+    Transform target;
+    public float moveSpeed = 5.0f;
+    private float delta = Time.deltaTime;
+    Vector3 forward = new Vector3(0, 0, -1);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target.position, forward);
+        transform.Rotate(new Vector3(0, -90, 0), Space.Self);
+        if (Vector3.Distance(transform.position, target.position) > 1f)
+        {
+            transform.Translate(new Vector3(moveSpeed * Time.deltaTime, 0, 0));
+        }
+    }
+
+}
