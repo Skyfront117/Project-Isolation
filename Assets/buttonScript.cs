a5ad6c95a4bc22a25388066e489e873bf05004ef
GPalau-Ribes <55253900+GPalau-Ribes@users.noreply.github.com>
2020-01-31 18:20:24 +0100

    Movement test1
    test de movimiento num1.

---
+++ Assets/buttonScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonScript : MonoBehaviour
+{
+    public void salir()
+    {
+        Application.Quit();
+    }
+}
