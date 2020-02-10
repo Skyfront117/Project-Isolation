ca26353925f518235546f5b56719fbf25b2b3e2f
GPalau-Ribes <55253900+GPalau-Ribes@users.noreply.github.com>
2020-02-10 23:09:35 +0100

    v 0.6
    FINISHED:
    - SHOOTING,
    - ITEMS,
    - ENEMY,
    - PLAYER MOVEMENT.

--- Assets/scripts/EnemiesMovement.cs
+++ Assets/scripts/EnemiesMovement.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class EnemiesMovement : MonoBehaviour
 {
     Transform target;
-    float moveSpeed = 50.0f;
+    private float moveSpeed = 50.0f;
+    private const int startHP = 4;
+    private int actualHP = startHP;
+    private bool stunned;
+
+    float timerA = 0;
+    float timerB = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +22,41 @@ public class EnemiesMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target.position);
-        transform.Rotate(new Vector3(0, -90, 0), Space.Self);
-        if (Vector3.Distance(transform.position, target.position) > 1.215f)
+        timerB += Time.deltaTime;
+        if (actualHP > 0)
         {
-            transform.Translate(new Vector3(moveSpeed * Time.deltaTime, 0, 0));
+            transform.LookAt(target.position);
+            transform.Rotate(new Vector3(0, -90, 0), Space.Self);
+            if (Vector3.Distance(transform.position, target.position) > 1.215f)
+            {
+                transform.Translate(new Vector3(moveSpeed * Time.deltaTime, 0, 0));
+            }
         }
+        if (stunned)
+        {
+            if ((timerB - timerA) > 4)
+            {
+                stunned = false;
+                actualHP = startHP;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!stunned)
+        {
+            if (collision.gameObject.tag == "Bullet")
+            {
+                actualHP--;
+                if (actualHP < 1)
+                {
+                    stunned = true;
+                    timerA = 0;
+                    timerB = 0;
+                }
+            }
+        }
+        
     }
 }
