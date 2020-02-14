e26cb708f54034998e0477b06650fa356e6fbf88
Skyfront117 <guillemlali@gmail.com>
2020-02-14 16:24:46 +0100

    Merged playerControllers

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -8,75 +8,84 @@ public class playerController : MonoBehaviour
     public GameObject bulletSpawner;
     public AudioSource audioSource;
     public Animator animator;
-    public Rigidbody2D rigidBody;
-
+    Rigidbody2D rigidBody;
     public GameObject bullet;
     Vector3 originalCameraPosition;
 
-    public float fireRate = 0.6f;
+    private float fireRate = 0.3f;
     float timeA = 0;
     float timeB = 0;
     float bulletThrust = 800;
-    float shakeAmt = 0;
 
     float speed = 90.0f;
-    public Vector2 position = new Vector2(0, 0);
-    public Vector3 cameraPosition = new Vector3(0, 0, -10);
+    private Vector2 position = new Vector2(0, 0);
+    private Vector3 cameraPosition = new Vector3(0, 0, -10);
+
+    public int HP;
 
     private Vector3 mouse = new Vector3(0, 0, 0);
+
     private void Start()
     {
         animator.GetBool("moving");
         audioSource = GetComponent<AudioSource>();
         rigidBody = GetComponent<Rigidbody2D>();
+        HP = 4;
     }
+
     private void Update()
     {
         timeB += Time.deltaTime;
-        if (Input.GetMouseButtonDown(0))
+        if (HP > 0)
         {
-            if ((timeB - timeA) > fireRate)
+            if (Input.GetMouseButtonDown(0))
             {
-                GameObject temporalBullet = Instantiate(bullet, bulletSpawner.transform.position, transform.rotation);
-                temporalBullet.GetComponent<Rigidbody2D>().AddForce(transform.up * bulletThrust, ForceMode2D.Impulse);
-                timeA = timeB;
-                audioSource.Play(0);
+                if ((timeB - timeA) > fireRate)
+                {
+                    GameObject temporalBullet = Instantiate(bullet, bulletSpawner.transform.position, transform.rotation);
+                    temporalBullet.GetComponent<Rigidbody2D>().AddForce(transform.up * bulletThrust, ForceMode2D.Impulse);
+                    timeA = timeB;
+                    audioSource.Play(0);
+                }
             }
         }
     }
 
     void FixedUpdate()
     {
-        mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (HP > 0)
+        {
+            if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D))
+            {
+                animator.SetBool("moving", false);
+            }
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            position.y += speed * Time.fixedDeltaTime;
-            animator.SetBool("moving", true);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            position.x -= speed * Time.fixedDeltaTime;
-            animator.SetBool("moving", true);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            position.y -= speed * Time.fixedDeltaTime;
-            animator.SetBool("moving", true);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            position.x += speed * Time.fixedDeltaTime;
-            animator.SetBool("moving", true);
-        }
-        if (!Input.GetKey(KeyCode.W)&& !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D))
-        {
-            animator.SetBool("moving", false);
-        }
+            mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(mouse.y - transform.position.y, mouse.x - transform.position.x) * Mathf.Rad2Deg - 90);
+            if (Input.GetKey(KeyCode.W))
+            {
+                position.y += speed * Time.fixedDeltaTime;
+                animator.SetBool("moving", true);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                position.x -= speed * Time.fixedDeltaTime;
+                animator.SetBool("moving", true);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                position.y -= speed * Time.fixedDeltaTime;
+                animator.SetBool("moving", true);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                position.x += speed * Time.fixedDeltaTime;
+                animator.SetBool("moving", true);
+            }
+            transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(mouse.y - transform.position.y, mouse.x - transform.position.x) * Mathf.Rad2Deg - 90);
 
-        transform.position = position;
+            transform.position = position;
+        }
         cameraPosition.x = position.x;
         cameraPosition.y = position.y;
         mainCamera.transform.position = cameraPosition;
