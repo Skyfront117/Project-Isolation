6d5738e81cf5caf6a8842afed270fb1e29b89e5d
malloryxdd <paumedinamartin@gmail.com>
2020-03-22 13:33:14 +0100

    pathfinding
    hace el pathfinding pero no evita ir por donde hay gameobjects de la layer indicada.

---
+++ Assets/Scripts/EnemyIA.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyIA : MonoBehaviour
+{
+
+    public Transform target;
+
+    private float enemySpeed;
+    private float nextWaypoint;
+
+    private Path path;
+    private int currentWaypoint;
+    private bool reachEnd;
+
+    private Seeker seeker;
+    private Transform enemy;
+    private Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemySpeed = 2500f;
+        nextWaypoint = 3f;
+        reachEnd = false;
+        currentWaypoint = 0;
+        enemy = GetComponent<Transform>();
+        seeker = GetComponent<Seeker>();
+        InvokeRepeating("UpdatePath", 0f, 0.5f);
+        seeker.StartPath(enemy.position, target.position, onPathComplete);
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void onPathComplete(Path _path)
+    {
+        if (!_path.error)
+        {
+            path = _path;
+            currentWaypoint = 0;
+        }
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+        {
+            seeker.StartPath(enemy.position, target.position, onPathComplete);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(path == null) { return; }
+        if(currentWaypoint >= path.vectorPath.Count)
+        {
+            reachEnd = true;
+            return;
+        }
+        else
+        {
+            reachEnd = false;
+        }
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - (Vector2)enemy.position).normalized;
+        Vector2 Force = direction * enemySpeed * Time.fixedDeltaTime;
+
+        rb2d.velocity = Force;
+
+        float distance = Vector2.Distance(enemy.position, path.vectorPath[currentWaypoint]);
+        if(distance < nextWaypoint)
+        {
+            currentWaypoint++;
+        }
+    }
+}
