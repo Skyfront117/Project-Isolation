bb03cb094dab36388bb42d148434de42b462f439
GPalau-Ribes <55253900+GPalau-Ribes@users.noreply.github.com>
2020-03-22 21:05:56 +0100

    aefs
    as

---
+++ Assets/Scripts/TutorialEnemy1Script.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialEnemy1Script : MonoBehaviour
+{
+    private int HP = 4;
+    private GameObject player;
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+        rb = GetComponent<Rigidbody2D>();
+    }
+    //transform.Rotate(new Vector3(0, -90, 0), Space.Self);
+
+    private void Update()
+    {
+        Vector2 lookDir = player.transform.position - transform.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle; 
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            HP--;
+            if (HP < 1)
+            {
+                TutorialManager.Instance.enemiesCount--;
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/TutorialManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public static TutorialManager Instance { get; private set; }
+    public int enemiesCount = 4;
+    private enum tutorialPhase { staticEnemies, movingEnemies, normalEnemies, levelLearning};
+    private tutorialPhase actualPhase = tutorialPhase.staticEnemies;
+
+    public GameObject TutorialEnemy2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (actualPhase)
+        {
+            case tutorialPhase.staticEnemies:
+                if (enemiesCount < 1)
+                {
+                    actualPhase++;
+                }
+                break;
+            case tutorialPhase.movingEnemies:
+                if (enemiesCount < 1)
+                {
+                    actualPhase++;
+                }
+                break;
+            case tutorialPhase.normalEnemies:
+                break;
+            case tutorialPhase.levelLearning:
+                break;
+            default:
+                break;
+        }
+    }
+}
