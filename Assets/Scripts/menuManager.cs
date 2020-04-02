c6bc26bd6d759e585c6ce451de6c61251baa3665
malloryxdd <paumedinamartin@gmail.com>
2020-04-02 14:15:08 +0200

    quit and play button

--- Assets/Scripts/menuManager.cs
+++ Assets/Scripts/menuManager.cs
@@ -6,25 +6,51 @@ using UnityEngine.SceneManagement;
 
 public class menuManager : MonoBehaviour
 {
-    public Button play;
-    public Button exit;
+    public bool play;
+    private bool options;
+    private bool credits;
+    private bool exit;
     void Start()
     {
-
+        play = false;
+        exit = false;
+        credits = false;
+        options = false;
     }
 
     void Update()
     {
-
+        if (play)
+        {
+            play = false;
+            exit = false;
+            credits = false;
+            options = false;
+            SceneManager.LoadScene("Tutorial");
+        }
+        if (exit)
+        {
+            play = false;
+            exit = false;
+            credits = false;
+            options = false;
+            Application.Quit();
+        }
     }
 
     public void onClickPlay()
     {
-        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+        play = true;
+        exit = false;
+        credits = false;
+        options = false;
     }
 
     public void onClickClose()
     {
-        Application.Quit();
+        play = false;
+        exit = true;
+        credits = false;
+        options = false;
     }
 }
