38d4b4fd15e86c52e602bf11c7bd38256e60dd92
malloryxdd <paumedinamartin@gmail.com>
2020-05-12 18:17:52 +0200

    Main Menu buttons adjust

--- Assets/Scripts/menuManager.cs
+++ Assets/Scripts/menuManager.cs
@@ -29,7 +29,7 @@ public class menuManager : MonoBehaviour
             exit = false;
             credits = false;
             options = false;
-            SceneManager.LoadScene("Level1");
+            SceneManager.LoadScene("Level2");
         }else
         if (exit)
         {
