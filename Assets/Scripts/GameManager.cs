93ad1572f10f1e61e25427e590906b33879aa459
malloryxdd <paumedinamartin@gmail.com>
2020-05-14 09:21:09 +0200

    Game manager level selector + save progress

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance { get; private set; }
+
+    public int levelNum;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelNum = 0;
+        BinaryReader reader;
+        if (File.Exists("save.sav"))
+        {
+            reader = new BinaryReader(File.Open("save.sav", FileMode.Open));
+            levelNum = reader.ReadInt32();
+            reader.Close();
+        }
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void saveProgress()
+    {
+        BinaryWriter writer = new BinaryWriter(File.Open("save.sav", FileMode.Create));
+        writer.Write(levelNum);
+        writer.Close();
+    }
+}
