ab0542dac4824352f740b04c7c3e440b79e2f568
Enekai <gerardpr2001@gmail.com>
2020-05-14 19:18:39 +0200

    idk
    idk

---
+++ Assets/TutorialCircleScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialCircleScript : MonoBehaviour
+{
+    GameObject player;
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Level1");
+        }
+    }
+}
