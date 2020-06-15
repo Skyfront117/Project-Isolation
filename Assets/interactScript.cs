c178c401117dd7e0f1646cc13c58e0fa6a50578d
Skyfront117 <guillemlali@gmail.com>
2020-06-15 22:32:45 +0200

    interactScript

---
+++ Assets/interactScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class interactScript : MonoBehaviour
+{
+    public static interactScript instance { get; private set; }
+    public bool canInteract;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        canInteract = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canInteract)
+        {
+            GetComponent<Text>().text = "E TO INTERACT";
+        }
+        else
+        {
+            GetComponent<Text>().text = "";
+        }
+    }
+}
