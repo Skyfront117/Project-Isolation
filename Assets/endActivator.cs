b858ad50130ca90250691f70bb0d5e90b3370ee5
Skyfront117 <guillemlali@gmail.com>
2020-06-16 07:58:38 +0200

    level logic

--- Assets/endActivator.cs
+++ Assets/endActivator.cs
@@ -18,7 +18,7 @@ public class endActivator : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player" && LevelManager.Instance.level1 == true)
+        if (collision.gameObject.tag == "Player" && LevelManager.Instance.level1 && !LevelManager.Instance.getCanEnd())
         {
             Debug.Log("level 3 completed");
             LevelManager.Instance.setEnd(true);
---
+++ Assets/level2Activator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class level2Activator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && LevelManager.Instance.level1 && !LevelManager.Instance.level2)
+        {
+            Debug.Log("level 2 completed");
+            LevelManager.Instance.level2 = true;
+        }
+        else if (collision.gameObject.tag == "Player" && !LevelManager.Instance.level1)
+        {
+            Debug.Log("level 1 not completed");
+        }
+    }
+}
