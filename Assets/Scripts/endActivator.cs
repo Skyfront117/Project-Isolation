cd1db48aa2188024dc72a9885f4ed5a27e85185a
Enekai <gerardpr2001@gmail.com>
2020-06-16 18:49:09 +0200

    1rst console
    1rst console

---
+++ Assets/Scripts/endActivator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endActivator : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && LevelManager.Instance.level1 && !LevelManager.Instance.getCanEnd())
+        {
+            LevelManager.Instance.setEnd(true);
+        }
+    }
+}
